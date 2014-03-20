431ec88ce38f6983ec8f4224603121f696dc747a
小林 幸司 <cozynooks@gmail.com>
2014-03-20 17:14:04 +0900

    Add zip extension in configure option

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,41 @@
-default['php']['version']    = '5.5.9'
-default['httpd']['version']      = '2.4.7'
+default['php']['version']   = '5.5.9'
+default['php']['src_dir']    = "/usr/src"
+default['php']['prefix_dir'] = "/opt/php/#{php['version']}"
 
-default['httpd']['src_dir']    = "/usr/src"
+default['httpd']['version'] = '2.4.7'
 default['httpd']['prefix_dir'] = "/opt/httpd/#{httpd['version']}"
+
+default['php']['configure_options'] = %W{--prefix=/opt/php/5.5.9
+                                            --with-libdir=lib64
+                                            --with-apxs2=/opt/httpd/2.4.7/bin/apxs
+                                            --enable-mbstring
+                                            --enable-intl
+                                            --with-icu-dir=/usr
+                                            --with-gettext=/usr
+                                            --with-pcre-regex=/opt/pcre/8.34
+                                            --with-readline=/usr
+                                            --with-libxml-dir=/usr/bin/xml2-config
+                                            --enable-soap
+                                            --enable-wddx
+                                            --with-xmlrpc
+                                            --with-xsl=/usr
+                                            --with-mysql=mysqlnd
+                                            --with-mysqli=mysqlnd
+                                            --with-pdo-mysql=mysqlnd
+                                            --with-pgsql=/opt/postgresql/9.3.3
+                                            --with-pdo-pgsql=/opt/postgresql/9.3.3
+                                            --with-zlib=/usr
+                                            --with-zlib-dir=/usr
+                                            --enable-dba
+                                            --with-db4=/usr
+                                            --with-gd
+                                            --with-jpeg-dir=/usr
+                                            --with-png-dir=/usr
+                                            --with-freetype-dir=/usr
+                                            --enable-gd-native-ttf
+                                            --enable-gd-jis-conv
+                                            --with-openssl=/usr
+                                            --with-mcrypt=/usr
+                                            --with-zip=/usr
+                                            --enable-bcmath}
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -15,41 +15,50 @@ pkgs = %w{
   libpng-devel
   freetype-devel
   libicu-devel
-  libmcrypt-devel
   readline-devel
   libxslt-devel
-}
+}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
 
-default['php']['configure_options'] = %W{--prefix=/opt/php/5.5.9
-                                            --with-libdir=lib64
-                                            --with-apxs2=/opt/httpd/2.4.7/bin/apxs
-                                            --enable-mbstring
-                                            --enable-intl
-                                            --with-icu-dir=/usr
-                                            --with-gettext=/usr
-                                            --with-pcre-regex=/opt/pcre/8.34
-                                            --with-readline=/usr
-                                            --with-libxml-dir=/usr/bin/xml2-config
-                                            --enable-soap
-                                            --enable-wddx
-                                            --with-xmlrpc
-                                            --with-xsl=/usr
-                                            --with-mysql=mysqlnd
-                                            --with-mysqli=mysqlnd
-                                            --with-pdo-mysql=mysqlnd
-                                            --with-pgsql=/opt/postgresql-9.3.3 \
-                                            --with-pdo-pgsql=/opt/postgresql-9.3.3 \
-                                            --with-zlib=/usr
-                                            --with-zlib-dir=/usr
-                                            --enable-dba
-                                            --with-db4=/usr
-                                            --with-gd
-                                            --with-jpeg-dir=/usr
-                                            --with-png-dir=/usr
-                                            --with-freetype-dir=/usr
-                                            --enable-gd-native-ttf
-                                            --enable-gd-jis-conv
-                                            --with-openssl=/usr
-                                            --with-mcrypt=/usr
-                                            --enable-bcmath}
+template "epel.repo" do
+  path "/etc/yum.repos.d/epel.repo"
+  source "epel.repo.erb"
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+bash "install libmcrypt" do
+  user "root"
+  cwd "/usr/src"
+  code <<-EOF
+  wget http://ftp.riken.jp/Linux/fedora/epel/RPM-GPG-KEY-EPEL-6
+  rpm --import RPM-GPG-KEY-EPEL-6
+  rm -f RPM-GPG-KEY-EPEL-6
+  yum --enablerepo=epel -y install libmcrypt-devel
+  EOF
+end
+
+configure_options = node['php']['configure_options'].join(" ")
+
+cookbook_file "#{node['php']['src_dir']}/php-#{node['php']['version']}.tar.gz" do
+  mode 0644
+end
+
+bash "build php" do
+  user "root"
+  cwd node['php']['src_dir']
+  code <<-EOF
+  tar -zxvf php-#{node['php']['version']}.tar.gz
+  cd php-#{node['php']['version']}
+  ./configure #{configure_options}
+  make
+  make install
+  EOF
+  not_if "ls #{node['php']['prefix_dir']}"
+end
+
 
